f8c2e48b25b70f290e79a804ac3cc8df51964399
Aksharakeerthi <akshara2223@gmail.com>
2017-07-06 18:04:04 +0530

    Create Nila.java

---
+++ Nila.java
@@ -0,0 +1,19 @@
+# Nila
+import java.io.*;
+import java.util.*;
+public class Nila{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int k;
+System.out.println("Enter rthe number");
+k=sc.nextInt();
+if(n>0){
+System.out.println("Number is positive");
+}
+else if(n<0){
+System.out.println("Number is negative");
+}
+else
+System.out.println("Number is zero");
+}
+}
